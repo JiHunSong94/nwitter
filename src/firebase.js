a5c30334f3b5a9fdff17f8529fe66d3ed743d3b1
Jihun Song <songjihun940409@gmail.com>
2022-07-05 16:30:57 +0900

    #1.1 SETUP - Firebase Setup

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUrgEpMPQXVkpk3s5Cd4kjRZ0ENdhjODE",
+  authDomain: "nwitter-f17ab.firebaseapp.com",
+  projectId: "nwitter-f17ab",
+  storageBucket: "nwitter-f17ab.appspot.com",
+  messagingSenderId: "106618017947",
+  appId: "1:106618017947:web:ae0840c3f2bbac863a4e3e",
+};
+const app = initializeApp(firebaseConfig);
+
+export default app;
