70e3589b6f47bd7fcba3f96eb6f6f8fd1e46f364
Jihun Song <songjihun940409@gmail.com>
2022-07-07 20:21:28 +0900

    #2.3 AUTH - Log In

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "./Router";
 import { getAuth } from "firebase/auth";
 
 function App() {
   const auth = getAuth();
-  const [isLoggedIn, setIsLoggedIn] = useState(auth.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
