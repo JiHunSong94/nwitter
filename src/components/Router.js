534da8755b06f3c7da3c70012afe28ddec311bb5
Jihun Song <songjihun940409@gmail.com>
2022-07-09 00:50:34 +0900

    #2.5 AUTH - Log Out

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,31 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "../routes/Home";
 import Auth from "../routes/Auth";
+import Profile from "../routes/Profile";
+import Navigation from "./Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
-          <Route exact path="/" element={<Home />} />
+          <>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/profile" element={<Profile />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </>
         ) : (
-          <Route exact path="/" element={<Auth />} />
+          <>
+            <Route exact path="/" element={<Auth />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </>
         )}
       </Routes>
     </Router>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,4 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { getAuth } from "firebase/auth";
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+  const auth = getAuth();
+  let navigate = useNavigate();
+  const onLogOutClick = () => {
+    auth.signOut();
+    navigate("/", { replace: true });
+  };
+
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
+
 export default Profile;
