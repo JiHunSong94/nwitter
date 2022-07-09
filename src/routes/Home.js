1e00da3779611024d1053dd3fe57d74517252d55
Jihun Song <songjihun940409@gmail.com>
2022-07-10 02:27:37 +0900

    #3.0 NWEETING -Creating Form For Nweeting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+        />
+        <input type="submit" value="nweet" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
