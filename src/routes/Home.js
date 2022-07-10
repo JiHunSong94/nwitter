f981fd48b145888d5993354cea3afb242b7172fa
Jihun Song <songjihun940409@gmail.com>
2022-07-11 00:41:00 +0900

    #3.1 NWEETING - Nweeting!

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
+import { db } from "../firebase";
+import { collection, addDoc } from "firebase/firestore";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
-
   const onChange = (event) => {
     const {
       target: { value },
     } = event;
     setNweet(value);
   };
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+    await addDoc(collection(db, "nweets"), {
+      nweet,
+      createdAt: Date.now(),
+    });
+    setNweet("");
   };
   return (
     <div>
