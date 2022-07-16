c699b1b49d1d95e0153c87c14fc16a7a25680c25
Jihun Song <songjihun940409@gmail.com>
2022-07-16 13:43:51 +0900

    #5.0 EDIT PROFILE - Get My Own Nweets

--- src/components/Router.js
+++ src/components/Router.js
@@ -18,7 +18,11 @@ const AppRouter = ({ isLoggedIn, userObj }) => {
         {isLoggedIn ? (
           <>
             <Route exact path="/" element={<Home userObj={userObj} />} />
-            <Route exact path="/profile" element={<Profile />} />
+            <Route
+              exact
+              path="/profile"
+              element={<Profile userObj={userObj} />}
+            />
             <Route path="*" element={<Navigate to="/" replace />} />
           </>
         ) : (
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { getAuth } from "firebase/auth";
+import { db } from "../firebase";
+import { collection, query, where, getDocs, orderBy } from "firebase/firestore";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const auth = getAuth();
   let navigate = useNavigate();
   const onLogOutClick = () => {
     auth.signOut();
     navigate("/", { replace: true });
   };
+  const getMyNweets = async () => {
+    const q = query(
+      collection(db, "nweets"),
+      where("nweetObj.creatorId", "==", userObj.uid),
+      orderBy("nweetObj.createdAt", "desc")
+    );
+    const data = await getDocs(q);
+    console.log(data.docs.map((doc) => doc.data()));
+  };
+  useEffect(() => {
+    getMyNweets();
+  }, []);
 
   return (
     <>
